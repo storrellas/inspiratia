75431262beb6696e200e1f1a5f85711a41cc8c43
storrellas <storrellas@gmail.com>
2026-07-07 09:56:32 +0200

    feat: remove react-animate-height

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ import Image from "next/image";
 import Link from "next/link";
 import ReactPaginate from 'react-paginate';
 import "@fortawesome/fontawesome-free/css/all.min.css";
-import AnimateHeight from "react-animate-height";
 
 
 class Item {
@@ -22,13 +21,98 @@ class Item {
   }
 }
 
+const Filtering = ({ onFilter, onReset }: { onFilter: (filter: Item) => void, onReset: () => void }) => {
+  const [filter, setFilter] = useState<Item>(new Item(null, null, "", ""));
+
+  const onFilterLocal = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    onFilter(filter);
+  }
+
+  const onResetLocal = () => {
+    setFilter(new Item(null, null, "", ""));
+    onReset();
+  }
+
+  return (
+    <form onSubmit={onFilterLocal}>
+      <div className="flex justify-between">   
+        <h1 className="text-xl font-bold mb-2">Filtering</h1>
+      </div>
+      <div className="gap-3">
+        <div className="flex gap-3 w-full">
+          <div className="w-1/2">
+            <label htmlFor="filter" className="block text-sm font-medium text-gray-700 mb-1">
+              ID:
+            </label>
+            <input type="number"  placeholder="Filter by ID..." value={filter.id !== null ? filter.id : ""}
+              className="border border-gray-300 rounded px-3 py-2 w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
+              onChange={(e) => {
+                const filterText = e.target.value.toLowerCase();
+                setFilter({ ...filter, id: Number(filterText) });
+              }}
+            />
+          </div>
+          <div className="w-1/2">
+            <label htmlFor="filter" className="block text-sm font-medium text-gray-700 mb-1">
+              User ID:
+            </label>
+            <input type="number" placeholder="Filter by user ID..." value={filter.userId !== null ? filter.userId : ""}
+              className="border border-gray-300 rounded px-3 py-2 w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
+              onChange={(e) => {
+                const filterText = e.target.value.toLowerCase();
+                setFilter({ ...filter, userId: Number(filterText) });                  
+              }}
+            />
+          </div>
+        </div>
+
+        <div className="flex gap-3 w-full mt-3">
+          <div className="w-1/2">
+            <label htmlFor="filter" className="block text-sm font-medium text-gray-700 mb-1">
+              Filter by title:
+            </label>
+            <input type="text"  placeholder="Filter by title..." value={filter.title}
+              className="border border-gray-300 rounded px-3 py-2 w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
+              onChange={(e) => {
+                const filterText = e.target.value.toLowerCase();
+                setFilter({ ...filter, title: filterText });
+              }}
+            />
+          </div>
+          <div className="w-1/2">
+            <label htmlFor="filter" className="block text-sm font-medium text-gray-700 mb-1">
+              Filter by body:
+            </label>
+            <input type="text" placeholder="Filter by body..." value={filter.body}
+              className="border border-gray-300 rounded px-3 py-2 w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
+              onChange={(e) => {
+                const filterText = e.target.value.toLowerCase();
+                setFilter({ ...filter, body: filterText });                  
+              }}
+            />
+          </div>
+        </div>
+      </div>
+      <div className="text-right mt-3">
+        <button type="button"
+          className="cursor-pointer bg-gray-600 text-white px-4 py-2 rounded hover:bg-gray-600 transition-colors mr-2" onClick={onResetLocal}>
+          Reset
+        </button>
+        <button type="submit"
+          className=" cursor-pointer inspiratia-bg-color text-white px-4 py-2 rounded hover:bg-blue-600 transition-colors">
+          Filter
+        </button>
+      </div>
+    </form> 
+  );
+}
 
 export default function Home() {
   const data = useRef<Item[]>([]);
-  const [dataFiltered, setDataFiltered]  = useState<Item[]>([]);  
   const dataFilteredRef = useRef<Item[]>([]);
   const [dataDisplay, setDataDisplay] = useState<{ data: Item[]; page: number, count: number }>({ data: [], page: 1, count: 0 });
-  const [filter, setFilter] = useState<Item>(new Item(null, null, "", ""));
+  // const [filter, setFilter] = useState<Item>(new Item(null, null, "", ""));
   const currentPageRef = useRef(0);
   const [showFilter, setShowFilter] = useState(false);
   const [pageSize, setPageSize] = useState(10);
@@ -36,14 +120,11 @@ export default function Home() {
   // ------------------------------
   // Handlers
   // ------------------------------
-  const onFilter = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
+  const onFilter = (filter: Item) => {
     let dataFullLocal = JSON.parse(JSON.stringify(data.current));
     console.log("Filtering data with filter:", filter);
-    if( filter.id !== null && filter.id != 0 ) {
-      dataFullLocal = dataFullLocal.filter((item: Item) => item.id === filter.id);
-    }
+    if( filter.id !== null && filter.id != 0 )
+      dataFullLocal = dataFullLocal.filter((item: Item) => item.id === filter.id);    
     if( filter.userId !== null && filter.userId != 0 )
       dataFullLocal = dataFullLocal.filter((item: Item) => item.userId === filter.userId);
     if( filter.title.length > 0)
@@ -59,8 +140,6 @@ export default function Home() {
   };
 
   const onReset = () => {
-    setFilter(new Item(null, null, "", ""));
-    // setDataFiltered(data.current);
     dataFilteredRef.current = data.current;
     setDataDisplay({ data: data.current.slice(0, pageSize), page: 1, count: data.current.length });
     currentPageRef.current = 0; // Reset to first page
@@ -104,82 +183,7 @@ export default function Home() {
           <div className={`grid transition-[grid-template-rows] duration-300 ease-in-out ${showFilter ? "grid-rows-[1fr]" : "grid-rows-[0fr]"}`}>
             <div className="overflow-hidden">
               <div className="rounded-lg border border-gray-200 shadow-sm p-5 mb-4 overflow-hidden">              
-                <form onSubmit={onFilter}>
-                <div className="flex justify-between">   
-                <h1 className="text-xl font-bold mb-2">Filtering</h1>
-                  <div className="text-right">
-                    <div className="inline-block text-gray-600 text-lg font-semibold mb-2 cursor-pointer" onClick={() => setShowFilter(!showFilter)}>
-                      <i className="fa-solid fa-times"></i>
-                    </div>            
-                  </div>
-                </div>
-                <div className="gap-3">
-                
-                  <div className="flex gap-3 w-full">
-                    <div className="w-1/2">
-                      <label htmlFor="filter" className="block text-sm font-medium text-gray-700 mb-1">
-                        ID:
-                      </label>
-                      <input type="number"  placeholder="Filter by ID..." value={filter.id !== null ? filter.id : ""}
-                        className="border border-gray-300 rounded px-3 py-2 w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
-                        onChange={(e) => {
-                          const filterText = e.target.value.toLowerCase();
-                          setFilter({ ...filter, id: Number(filterText) });
-                        }}
-                      />
-                    </div>
-                    <div className="w-1/2">
-                      <label htmlFor="filter" className="block text-sm font-medium text-gray-700 mb-1">
-                        User ID:
-                      </label>
-                      <input type="number" placeholder="Filter by user ID..." value={filter.userId !== null ? filter.userId : ""}
-                        className="border border-gray-300 rounded px-3 py-2 w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
-                        onChange={(e) => {
-                          const filterText = e.target.value.toLowerCase();
-                          setFilter({ ...filter, userId: Number(filterText) });                  
-                        }}
-                      />
-                    </div>
-                  </div>
-
-                  <div className="flex gap-3 w-full mt-3">
-                    <div className="w-1/2">
-                      <label htmlFor="filter" className="block text-sm font-medium text-gray-700 mb-1">
-                        Filter by title:
-                      </label>
-                      <input type="text"  placeholder="Filter by title..." value={filter.title}
-                        className="border border-gray-300 rounded px-3 py-2 w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
-                        onChange={(e) => {
-                          const filterText = e.target.value.toLowerCase();
-                          setFilter({ ...filter, title: filterText });
-                        }}
-                      />
-                    </div>
-                    <div className="w-1/2">
-                      <label htmlFor="filter" className="block text-sm font-medium text-gray-700 mb-1">
-                        Filter by body:
-                      </label>
-                      <input type="text" placeholder="Filter by body..." value={filter.body}
-                        className="border border-gray-300 rounded px-3 py-2 w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
-                        onChange={(e) => {
-                          const filterText = e.target.value.toLowerCase();
-                          setFilter({ ...filter, body: filterText });                  
-                        }}
-                      />
-                    </div>
-                  </div>
-                </div>
-                <div className="text-right mt-3">
-                  <button type="button"
-                    className="cursor-pointer bg-gray-600 text-white px-4 py-2 rounded hover:bg-gray-600 transition-colors mr-2" onClick={onReset}>
-                    Reset
-                  </button>
-                  <button type="submit"
-                    className=" cursor-pointer inspiratia-bg-color text-white px-4 py-2 rounded hover:bg-blue-600 transition-colors">
-                    Filter
-                  </button>
-                </div>
-                </form> 
+                <Filtering onFilter={onFilter} onReset={onReset} />
               </div>
             </div>              
           </div>
